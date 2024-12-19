cd130f9867d40cbb82b3ae888fa603dfcf55e229
harish-k05 <harishkece2026@gmail.com>
2024-12-19 22:16:52 +0530

    created the base for the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>hi iam harish and i am frontend developer </div>
+    </>
+  )
+}
+
+export default App
